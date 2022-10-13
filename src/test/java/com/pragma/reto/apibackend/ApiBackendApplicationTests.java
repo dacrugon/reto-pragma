f9c9eecb258a8303b3fbbf45c4e4685bfe3c0a7d
dacrugon <dario.gonzalez@pragma.com.co>
2022-10-13 09:52:00 -0500

    first commit

---
+++ src/test/java/com/pragma/reto/apibackend/ApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.reto.apibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
